3fce52f4052da36e964c18f58f75208a6631e0f1
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-07-05 17:01:27 +0600

    add cer

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const config = {
     connectionString: "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
     ssl: {
         rejectUnauthorized: true,
-        ca: fs.readFileSync(`${process.env.APPDATA}/postgresql/RootCA.crt`).toString(),
+        ca: fs.readFileSync(`postgresql/RootCA.cer`).toString(),
     },
 };
 
